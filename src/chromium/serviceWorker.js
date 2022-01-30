cf251c1ffdf10344f8eb1e0fbf96b0490e39be08
idrisssystem <idrisssystem@gmail.com>
2022-01-30 15:59:31 -0500

    Added Context Menus

--- src/chromium/serviceWorker.js
+++ src/chromium/serviceWorker.js
@@ -8,17 +8,30 @@ chrome.runtime.onMessage.addListener(
         }
     }
 );
+
+/*  Chromium Wallets
+    nkbihfbeogaeaoehlefnkodbefgpgknn - MetaMask
+    hnfanknocfeofbddgcijnmhnfnkdnaad - Coinbase
+    bfnaelmomeimhlpmgjnjophhpkkoljpa - Phantom
+    eajafomhmkipbjmfmhebemolkcicgfmd - Tally
+    bhhhlbepdkbapadjdnnojkbgioiodbic - Solflare
+    bgpipimickeadkjlklgciifhnalhdjhe - GeroWallet
+    fhbohimaelbohpjbbldcngcnapndodjp - Binance Wallet
+    aiifbnbfobpmeekipheeijimdpnlpgpp - TerraStation Wallet
+*/
+
+
 chrome.contextMenus.create({
     title: 'Open IDriss',
     contexts: ["page"],
     id: "idriss-crypto-1",
-    documentUrlPatterns: ["chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn/*", "chrome-extension://hnfanknocfeofbddgcijnmhnfnkdnaad/*"]
+    documentUrlPatterns: ["extension://ejbalbakoplchlghecdalmeeeajnimhm/*", "chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn/*", "chrome-extension://hnfanknocfeofbddgcijnmhnfnkdnaad/*", "chrome-extension://bfnaelmomeimhlpmgjnjophhpkkoljpa/*", "chrome-extension://eajafomhmkipbjmfmhebemolkcicgfmd/*", "chrome-extension://bhhhlbepdkbapadjdnnojkbgioiodbic/*", "chrome-extension://bgpipimickeadkjlklgciifhnalhdjhe/*", "chrome-extension://fhbohimaelbohpjbbldcngcnapndodjp/*", "chrome-extension://aiifbnbfobpmeekipheeijimdpnlpgpp/*"]
 });
 chrome.contextMenus.onClicked.addListener((info, tab) => {
     chrome.windows.create({url: '/popup.html', width: 450, height: 600, type: 'popup'})
 })
 chrome.tabs.onUpdated.addListener(async (tabId, _, tab) => {
-    let isActive = tab.url.includes("nkbihfbeogaeaoehlefnkodbefgpgknn") || tab.url.includes("hnfanknocfeofbddgcijnmhnfnkdnaad")
+    let isActive = tab.url.includes("ejbalbakoplchlghecdalmeeeajnimhm") || tab.url.includes("nkbihfbeogaeaoehlefnkodbefgpgknn") || tab.url.includes("hnfanknocfeofbddgcijnmhnfnkdnaad") || tab.url.includes("bfnaelmomeimhlpmgjnjophhpkkoljpa") || tab.url.includes("eajafomhmkipbjmfmhebemolkcicgfmd") || tab.url.includes("bhhhlbepdkbapadjdnnojkbgioiodbic") || tab.url.includes("bgpipimickeadkjlklgciifhnalhdjhe") || tab.url.includes("fhbohimaelbohpjbbldcngcnapndodjp") || tab.url.includes("aiifbnbfobpmeekipheeijimdpnlpgpp")
     chrome.action.setIcon({path: isActive ? "img/icon16.png" : "img/icon16bw.png", tabId}, () => { /* ... */
     });
 })
--- src/firefox/serviceWorker.js
+++ src/firefox/serviceWorker.js
@@ -8,17 +8,26 @@ browser.runtime.onMessage.addListener(
         }
     }
 );
+
+/*  Firefox Wallets
+    f8447b2f-6dfc-45ef-acea-dbe7d9828659 - MetaMask
+    92744c8c-9eb2-4c22-8d0c-f0fb8a0bb9ca - Phantom
+    b40d9360-a5a5-43e5-928d-cbe6a73b66a1 - Solflare
+    0b8be570-8dcc-4755-95b4-bb5da48fa031 - Binance Wallet
+*/
+
+
 browser.contextMenus.create({
     title: 'Open IDriss',
     contexts: ["page"],
     id: "idriss-crypto-1",
-    documentUrlPatterns: ["moz-extension://f8447b2f-6dfc-45ef-acea-dbe7d9828659/*"]
+    documentUrlPatterns: ["moz-extension://f8447b2f-6dfc-45ef-acea-dbe7d9828659/*", "moz-extension://92744c8c-9eb2-4c22-8d0c-f0fb8a0bb9ca/*", "moz-extension://b40d9360-a5a5-43e5-928d-cbe6a73b66a1/*", "moz-extension://0b8be570-8dcc-4755-95b4-bb5da48fa031/*"]
 });
 browser.contextMenus.onClicked.addListener((info, tab) => {
     chrome.windows.create({url: '/popup.html', width: 450, height: 600, type: 'popup'})
 })
 browser.tabs.onUpdated.addListener(async (tabId, _, tab) => {
-    let isActive = tab.url.includes("f8447b2f-6dfc-45ef-acea-dbe7d9828659")
+    let isActive = tab.url.includes("f8447b2f-6dfc-45ef-acea-dbe7d9828659") || tab.url.includes("92744c8c-9eb2-4c22-8d0c-f0fb8a0bb9ca") || tab.url.includes("b40d9360-a5a5-43e5-928d-cbe6a73b66a1") || tab.url.includes("0b8be570-8dcc-4755-95b4-bb5da48fa031")
     browser.action.setIcon({path: isActive ? "img/icon16.png" : "img/icon16bw.png", tabId}, () => { /* ... */
     });
 })
