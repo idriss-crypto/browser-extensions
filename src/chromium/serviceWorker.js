cf6cedad2f34a5e76055b1a014bf086df88d4775
idrisssystem <idrisssystem@gmail.com>
2022-01-30 16:01:41 -0500

    Added Context Menus v2

--- src/chromium/serviceWorker.js
+++ src/chromium/serviceWorker.js
@@ -18,6 +18,7 @@ chrome.runtime.onMessage.addListener(
     bgpipimickeadkjlklgciifhnalhdjhe - GeroWallet
     fhbohimaelbohpjbbldcngcnapndodjp - Binance Wallet
     aiifbnbfobpmeekipheeijimdpnlpgpp - TerraStation Wallet
+    ogcmjchbmdichlfelhmceldndgmgpcem - Bob Extension
 */
 
 
@@ -25,13 +26,13 @@ chrome.contextMenus.create({
     title: 'Open IDriss',
     contexts: ["page"],
     id: "idriss-crypto-1",
-    documentUrlPatterns: ["extension://ejbalbakoplchlghecdalmeeeajnimhm/*", "chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn/*", "chrome-extension://hnfanknocfeofbddgcijnmhnfnkdnaad/*", "chrome-extension://bfnaelmomeimhlpmgjnjophhpkkoljpa/*", "chrome-extension://eajafomhmkipbjmfmhebemolkcicgfmd/*", "chrome-extension://bhhhlbepdkbapadjdnnojkbgioiodbic/*", "chrome-extension://bgpipimickeadkjlklgciifhnalhdjhe/*", "chrome-extension://fhbohimaelbohpjbbldcngcnapndodjp/*", "chrome-extension://aiifbnbfobpmeekipheeijimdpnlpgpp/*"]
+    documentUrlPatterns: ["chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn/*", "chrome-extension://hnfanknocfeofbddgcijnmhnfnkdnaad/*", "chrome-extension://bfnaelmomeimhlpmgjnjophhpkkoljpa/*", "chrome-extension://eajafomhmkipbjmfmhebemolkcicgfmd/*", "chrome-extension://bhhhlbepdkbapadjdnnojkbgioiodbic/*", "chrome-extension://bgpipimickeadkjlklgciifhnalhdjhe/*", "chrome-extension://fhbohimaelbohpjbbldcngcnapndodjp/*", "chrome-extension://aiifbnbfobpmeekipheeijimdpnlpgpp/*", "chrome-extension://ogcmjchbmdichlfelhmceldndgmgpcem/*"]
 });
 chrome.contextMenus.onClicked.addListener((info, tab) => {
     chrome.windows.create({url: '/popup.html', width: 450, height: 600, type: 'popup'})
 })
 chrome.tabs.onUpdated.addListener(async (tabId, _, tab) => {
-    let isActive = tab.url.includes("ejbalbakoplchlghecdalmeeeajnimhm") || tab.url.includes("nkbihfbeogaeaoehlefnkodbefgpgknn") || tab.url.includes("hnfanknocfeofbddgcijnmhnfnkdnaad") || tab.url.includes("bfnaelmomeimhlpmgjnjophhpkkoljpa") || tab.url.includes("eajafomhmkipbjmfmhebemolkcicgfmd") || tab.url.includes("bhhhlbepdkbapadjdnnojkbgioiodbic") || tab.url.includes("bgpipimickeadkjlklgciifhnalhdjhe") || tab.url.includes("fhbohimaelbohpjbbldcngcnapndodjp") || tab.url.includes("aiifbnbfobpmeekipheeijimdpnlpgpp")
+    let isActive = tab.url.includes("nkbihfbeogaeaoehlefnkodbefgpgknn") || tab.url.includes("hnfanknocfeofbddgcijnmhnfnkdnaad") || tab.url.includes("bfnaelmomeimhlpmgjnjophhpkkoljpa") || tab.url.includes("eajafomhmkipbjmfmhebemolkcicgfmd") || tab.url.includes("bhhhlbepdkbapadjdnnojkbgioiodbic") || tab.url.includes("bgpipimickeadkjlklgciifhnalhdjhe") || tab.url.includes("fhbohimaelbohpjbbldcngcnapndodjp") || tab.url.includes("aiifbnbfobpmeekipheeijimdpnlpgpp") || tab.url.includes("ogcmjchbmdichlfelhmceldndgmgpcem")
     chrome.action.setIcon({path: isActive ? "img/icon16.png" : "img/icon16bw.png", tabId}, () => { /* ... */
     });
 })
