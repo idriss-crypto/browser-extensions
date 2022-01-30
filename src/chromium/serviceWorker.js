a78b8dca22457877d8a5b734bfe399dfb3ac5412
MS <idrisssystem@gmail.com>
2022-01-30 17:28:58 -0500

    1. Added more IDs to context menus

--- src/chromium/serviceWorker.js
+++ src/chromium/serviceWorker.js
@@ -19,6 +19,9 @@ chrome.runtime.onMessage.addListener(
     fhbohimaelbohpjbbldcngcnapndodjp - Binance Wallet
     aiifbnbfobpmeekipheeijimdpnlpgpp - TerraStation Wallet
     ogcmjchbmdichlfelhmceldndgmgpcem - Bob Extension
+    hmeobnfnfcmdkdcmlblgagmfpfboieaf - XDEFI Wallet
+    dlcobpjiigpikoobohmabehhmhfoodbb - Argent X StarkNet Wallet
+    fnjhmkhhmkbjkkabndcnnogagogbneec - Ronin Wallet
 */
 
 
@@ -26,13 +29,13 @@ chrome.contextMenus.create({
     title: 'Open IDriss',
     contexts: ["page"],
     id: "idriss-crypto-1",
-    documentUrlPatterns: ["chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn/*", "chrome-extension://hnfanknocfeofbddgcijnmhnfnkdnaad/*", "chrome-extension://bfnaelmomeimhlpmgjnjophhpkkoljpa/*", "chrome-extension://eajafomhmkipbjmfmhebemolkcicgfmd/*", "chrome-extension://bhhhlbepdkbapadjdnnojkbgioiodbic/*", "chrome-extension://bgpipimickeadkjlklgciifhnalhdjhe/*", "chrome-extension://fhbohimaelbohpjbbldcngcnapndodjp/*", "chrome-extension://aiifbnbfobpmeekipheeijimdpnlpgpp/*", "chrome-extension://ogcmjchbmdichlfelhmceldndgmgpcem/*"]
+    documentUrlPatterns: ["chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn/*", "chrome-extension://hnfanknocfeofbddgcijnmhnfnkdnaad/*", "chrome-extension://bfnaelmomeimhlpmgjnjophhpkkoljpa/*", "chrome-extension://eajafomhmkipbjmfmhebemolkcicgfmd/*", "chrome-extension://bhhhlbepdkbapadjdnnojkbgioiodbic/*", "chrome-extension://bgpipimickeadkjlklgciifhnalhdjhe/*", "chrome-extension://fhbohimaelbohpjbbldcngcnapndodjp/*", "chrome-extension://aiifbnbfobpmeekipheeijimdpnlpgpp/*", "chrome-extension://ogcmjchbmdichlfelhmceldndgmgpcem/*", "chrome-extension://hmeobnfnfcmdkdcmlblgagmfpfboieaf/*", "chrome-extension://dlcobpjiigpikoobohmabehhmhfoodbb/*", "chrome-extension://fnjhmkhhmkbjkkabndcnnogagogbneec/*"]
 });
 chrome.contextMenus.onClicked.addListener((info, tab) => {
     chrome.windows.create({url: '/popup.html', width: 450, height: 600, type: 'popup'})
 })
 chrome.tabs.onUpdated.addListener(async (tabId, _, tab) => {
-    let isActive = tab.url.includes("nkbihfbeogaeaoehlefnkodbefgpgknn") || tab.url.includes("hnfanknocfeofbddgcijnmhnfnkdnaad") || tab.url.includes("bfnaelmomeimhlpmgjnjophhpkkoljpa") || tab.url.includes("eajafomhmkipbjmfmhebemolkcicgfmd") || tab.url.includes("bhhhlbepdkbapadjdnnojkbgioiodbic") || tab.url.includes("bgpipimickeadkjlklgciifhnalhdjhe") || tab.url.includes("fhbohimaelbohpjbbldcngcnapndodjp") || tab.url.includes("aiifbnbfobpmeekipheeijimdpnlpgpp") || tab.url.includes("ogcmjchbmdichlfelhmceldndgmgpcem")
+    let isActive = tab.url.includes("nkbihfbeogaeaoehlefnkodbefgpgknn") || tab.url.includes("hnfanknocfeofbddgcijnmhnfnkdnaad") || tab.url.includes("bfnaelmomeimhlpmgjnjophhpkkoljpa") || tab.url.includes("eajafomhmkipbjmfmhebemolkcicgfmd") || tab.url.includes("bhhhlbepdkbapadjdnnojkbgioiodbic") || tab.url.includes("bgpipimickeadkjlklgciifhnalhdjhe") || tab.url.includes("fhbohimaelbohpjbbldcngcnapndodjp") || tab.url.includes("aiifbnbfobpmeekipheeijimdpnlpgpp") || tab.url.includes("ogcmjchbmdichlfelhmceldndgmgpcem") || tab.url.includes("hmeobnfnfcmdkdcmlblgagmfpfboieaf") || tab.url.includes("dlcobpjiigpikoobohmabehhmhfoodbb") || tab.url.includes("fnjhmkhhmkbjkkabndcnnogagogbneec")
     chrome.action.setIcon({path: isActive ? "img/icon16.png" : "img/icon16bw.png", tabId}, () => { /* ... */
     });
 })
