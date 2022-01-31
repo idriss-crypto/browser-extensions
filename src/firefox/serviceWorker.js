fccb7c91599f472d96ef8fa793625b49f9a89cef
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2022-01-31 11:58:06 +0100

    show contextmenu on every extnsion

--- src/firefox/serviceWorker.js
+++ src/firefox/serviceWorker.js
@@ -9,20 +9,11 @@ browser.runtime.onMessage.addListener(
     }
 );
 
-/*  Firefox Wallets
-    f8447b2f-6dfc-45ef-acea-dbe7d9828659 - MetaMask
-    92744c8c-9eb2-4c22-8d0c-f0fb8a0bb9ca - Phantom
-    b40d9360-a5a5-43e5-928d-cbe6a73b66a1 - Solflare
-    0b8be570-8dcc-4755-95b4-bb5da48fa031 - Binance Wallet
-    b7f2adfa-eee4-4e2e-a1dc-b6264f24087b - Ronin Wallet
-*/
-
-
 browser.contextMenus.create({
     title: 'Open IDriss',
     contexts: ["page"],
     id: "idriss-crypto-1",
-    documentUrlPatterns: ["moz-extension://f8447b2f-6dfc-45ef-acea-dbe7d9828659/*", "moz-extension://92744c8c-9eb2-4c22-8d0c-f0fb8a0bb9ca/*", "moz-extension://b40d9360-a5a5-43e5-928d-cbe6a73b66a1/*", "moz-extension://0b8be570-8dcc-4755-95b4-bb5da48fa031/*", "moz-extension://b7f2adfa-eee4-4e2e-a1dc-b6264f24087b/*"]
+    documentUrlPatterns: ["moz-extension://*/*"]
 });
 browser.contextMenus.onClicked.addListener((info, tab) => {
     browser.windows.create({url: '/popup.html', width: 450, height: 640, type: 'popup'})
