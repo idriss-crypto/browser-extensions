6c51624b45753dcc13ac5ea22738ff6a71ca127c
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2022-05-17 17:21:40 +0200

    trying to make tipping by web3

--- src/common/tipping/Tipping.js
+++ src/common/tipping/Tipping.js
@@ -4,7 +4,8 @@ import {TippingWaitingApproval} from "./tippingWaitingApproval";
 import {TippingMain} from "./tippingMain";
 import {TippingWaitingConfirmation} from "./tippingWaitingConfirmation";
 import {TippingSuccess} from "./tippingSuccess";
-
+import {TippingLogic} from "./TippingLogic";
+import Web3Modal from "web3modal"
 
 export class Tipping {
     constructor(identifier) {
@@ -15,16 +16,25 @@ export class Tipping {
         let popup = create('section.tipping-popup')
         this.div.shadowRoot.append(popup);
         popup.append(new TippingMain(identifier).html);
-        popup.addEventListener('sendMoney',()=>{
+        popup.addEventListener('sendMoney', async () => {
             popup.firstElementChild.remove();
             popup.append(new TippingWaitingApproval(identifier).html)
-            setTimeout(()=>{//dummy
+            console.log('ssssssssssssssss')
+            const web3Modal = new Web3Modal({
+                network: 'mainnet',
+                cacheProvider: false, // optional
+                providerOptions: TippingLogic.providerOptions, // required
+                disableInjectedProvider: false,
+            });
+            await web3Modal.clearCachedProvider();
+            let provider = await web3Modal.connect();
+            setTimeout(() => {//dummy
 
                 popup.firstElementChild.remove();
                 popup.append(new TippingWaitingConfirmation(identifier).html)
             }, 5000)
 
-            setTimeout(()=>{//dummy
+            setTimeout(() => {//dummy
 
                 popup.firstElementChild.remove();
                 popup.append(new TippingSuccess(identifier).html)
