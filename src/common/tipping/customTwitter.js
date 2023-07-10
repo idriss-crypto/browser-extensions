df202dc7ae4347778ffb40119e23e358d34edd7d
lennardevertz <levertz@idriss.xyz>
2023-07-10 13:42:29 +0200

    customwidget changes and utils refactor

--- src/common/tipping/customTwitter.js
+++ src/common/tipping/customTwitter.js
@@ -9,21 +9,22 @@ export class CustomWidget {
         this.div.attachShadow({mode: 'open'})
         this.div.shadowRoot.append(create('style', {text: css}));
 
+
         let popup = create('section.sendToAnyone-popup')
         this.div.shadowRoot.append(popup);
         popup.append(new CustomTwitter(data).html);
         popup.addEventListener('customEvent', async (e) => {
             console.log({e, data})
             let params = {
-                identifier: e.identifier ?? null,
-                recipient: data['recipient_address'] ?? null,
-                network: e.network ?? null,
-                assetType: e.assetType ?? null,
-                assetAddress: e.assetAddress ?? null,
-                tippingValue: e.amount ?? null,
-                token: e.token ?? null,
-                message: e.message ?? null,
-                input: e.input ?? null,
+                identifier: data["forwardRecipient"] ? e.identifier : null,
+                recipient: data['recipientAddress'] ? data['recipientAddress'][e.network] : null,
+                network: data['showNetworkSelection'] ? e.network : null,
+                assetType: data['showTokenSelection'] ? e.assetType : null,
+                assetAddress: data['showTokenSelection'] ? e.assetAddress : null,
+                tippingValue: data['showTokenSelection'] ? e.amount : null,
+                token: data['showTokenSelection'] ? e.token : null,
+                message: data['showMessageBox'] ? e.message : null,
+                input: data['showInput'] ? e.input : null,
                 back:'close'
             }
             window.open(data['hostURL'] + Object.entries(params).filter(([k, v]) => v).map(x => encodeURIComponent(x[0]) + '=' + encodeURIComponent(x[1])).join('&'))
