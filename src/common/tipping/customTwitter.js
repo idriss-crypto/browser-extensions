f1520357c0531aba4ce9253eeefad10bc4734837
lennardevertz <levertz@idriss.xyz>
2023-08-09 13:52:57 +0200

    test recipient address and add isGrantee handler

--- src/common/tipping/customTwitter.js
+++ src/common/tipping/customTwitter.js
@@ -15,18 +15,39 @@ export class CustomWidget {
         popup.append(new CustomTwitter(data).html);
         popup.addEventListener('customEvent', async (e) => {
             console.log({e, data})
-            let params = {
-                identifier: data["forwardRecipient"] ? e.identifier : null,
-                recipient: data['recipientAddress'] ? data['recipientAddress'][e.network] : null,
-                network: data['showNetworkSelection'] ? e.network : null,
-                assetType: data['showTokenSelection'] ? e.assetType : null,
-                assetAddress: data['showTokenSelection'] ? e.assetAddress : null,
-                tippingValue: data['showTokenSelection'] ? e.amount : null,
-                token: data['showTokenSelection'] ? e.token : null,
-                message: data['showMessageBox'] ? e.message : null,
-                input: data['showInput'] ? e.input : null,
-                back:'close'
+            let params;
+            if (data['isGrantee']) {
+                 params = {
+                    identifier: data["forwardRecipient"] ? e.identifier : null,
+                    recipient: data['recipientAddress'] ? data['recipientAddress'][e.network] : null,
+                    network: data['showNetworkSelection'] ? e.network : null,
+                    assetType: data['showTokenSelection'] ? e.assetType : null,
+                    assetAddress: data['showTokenSelection'] ? e.assetAddress : null,
+                    tippingValue: data['showTokenSelection'] ? e.amount : null,
+                    token: data['showTokenSelection'] ? e.token : null,
+                    isGrantee: data['isGrantee'],
+                    grantAddress: data['grantAddress'],
+                    projectId: data['projectId'],
+                    applicationIndex: data['applicationIndex'],
+                    message: null,
+                    input: null,
+                    back:'close'
+                }
+            } else {
+                params = {
+                    identifier: data["forwardRecipient"] ? e.identifier : null,
+                    recipient: data['recipientAddress'] ? data['recipientAddress'][e.network] : null,
+                    network: data['showNetworkSelection'] ? e.network : null,
+                    assetType: data['showTokenSelection'] ? e.assetType : null,
+                    assetAddress: data['showTokenSelection'] ? e.assetAddress : null,
+                    tippingValue: data['showTokenSelection'] ? e.amount : null,
+                    token: data['showTokenSelection'] ? e.token : null,
+                    message: data['showMessageBox'] ? e.message : null,
+                    input: data['showInput'] ? e.input : null,
+                    back:'close'
+                }
             }
+
             window.open(data['hostURL'] + Object.entries(params).filter(([k, v]) => v).map(x => encodeURIComponent(x[0]) + '=' + encodeURIComponent(x[1])).join('&'))
         })
     }
