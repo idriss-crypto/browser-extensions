4d48ad33ba45f5cd21847719a7a18218ce9cc7d1
geoist1 <slon.mateusz@gmail.com>
2024-02-16 00:14:35 +0200

    Adding experimental features toggle & adjusting global toggle querying

--- src/common/standalone.js
+++ src/common/standalone.js
@@ -1,15 +1,15 @@
 import {StandalonePageManager} from "./pageManagers/standalonePageManager";
 
 (new StandalonePageManager(document, document.url)).init()
-document.querySelector('[type="checkbox"]').onchange =async e => {
+document.querySelector('[id="globalToggle"]').onchange =async e => {
     chrome.storage.local.set({'enabled': e.target.checked})
     chrome.storage.local.set({'cacheInvalidate': +new Date()})
     const allStorage=await chrome.storage.local.get(null);
     Object.keys(allStorage).filter(x=>x.startsWith('cache[')).forEach(x=>chrome.storage.local.remove(x))
 }
 chrome.storage.local.get(['enabled'], r => {
-    document.querySelector('[type="checkbox"]').checked = r?.enabled ?? true;
+    document.querySelector('[id="globalToggle"]').checked = r?.enabled ?? true;
     setTimeout(() => {
-        delete document.querySelector('[type="checkbox"]').classList.remove('noTransition');
+        delete document.querySelector('[id="globalToggle"]').classList.remove('noTransition');
     }, 50);
 })
