f8c0594d375e6bd632729fbe5782ef8f576d8530
nemmtor <kacper0witas@gmail.com>
2024-06-20 23:52:11 +0200

    feat: storybook

---
+++ src/application/polymarket/components/progress/progress.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Progress } from './progress';
+
+const meta: Meta<typeof Progress> = {
+  title: 'application/polymarket/progress',
+  component: Progress,
+  argTypes: {
+    value: { control: { type: 'number', step: 0.01, min: 0, max: 1 } },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Progress>;
+
+export const Primary: Story = {
+  args: {
+    value: 0.5,
+  },
+};
---
+++ src/application/polymarket/components/vote-button/vote-button.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { OUTCOME } from 'application/polymarket/constants';
+
+import { VoteButton } from './vote-button';
+
+const meta: Meta<typeof VoteButton> = {
+  title: 'application/polymarket/vote-button',
+  component: VoteButton,
+  argTypes: {
+    children: { table: { disable: true } },
+    onClick: { table: { disable: true } },
+    className: { table: { disable: true } },
+    outcome: { control: 'select', options: Object.values(OUTCOME) },
+  },
+  render: (parameters) => {
+    return (
+      <div className="grid">
+        <VoteButton {...parameters}>{parameters.outcome}</VoteButton>
+      </div>
+    );
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof VoteButton>;
+
+export const Primary: Story = {
+  args: {
+    isActive: true,
+    outcome: OUTCOME.YES,
+    disabled: false,
+  },
+};
