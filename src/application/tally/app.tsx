c3d8ff4338ab9b12387a567d0d9efd594eb63212
Daniel Chutkowski <daniel.chutkowski@gmail.com>
2024-06-25 23:15:19 +0200

    removed tallyProvider residues

--- src/application/tally/app.tsx
+++ src/application/tally/app.tsx
@@ -3,8 +3,6 @@ import { useExtensionSettings } from 'shared/extension';
 import { useTwitterLocationInfo, useHandleToUsernameMap } from 'host/twitter';
 
 import { ProposalHandleContainer, ProposalMainContainer } from './widgets';
-import { TallyProvider } from './tally.context';
-
 export const App = () => {
   const { experimentalFeatures } = useExtensionSettings();
 
@@ -28,14 +26,10 @@ export const App = () => {
 
   return (
     <ErrorBoundary exceptionEventName="tally-runtime-error">
-      <TallyProvider>
-        <>
-          {tallyUserHandle ? (
-            <ProposalHandleContainer tallyName={tallyUserHandle} />
-          ) : null}
-          {isTwitterHomePage ? <ProposalMainContainer /> : null}
-        </>
-      </TallyProvider>
+      {tallyUserHandle ? (
+        <ProposalHandleContainer tallyName={tallyUserHandle} />
+      ) : null}
+      {isTwitterHomePage ? <ProposalMainContainer /> : null}
     </ErrorBoundary>
   );
 };
