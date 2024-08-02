a1516405cb70623b61c4e58b45f02c5b3d861e69
Daniel Chutkowski <daniel.chutkowski@gmail.com>
2024-08-02 17:22:23 +0200

    moved number of fetched approvals to const

--- src/application/agora/commands/get-agora-proposals.ts
+++ src/application/agora/commands/get-agora-proposals.ts
@@ -19,6 +19,7 @@ interface Response {
 }
 
 const DISPLAYED_PROPOSAL_STATUS = 'ACTIVE';
+const NUMBER_OF_FETCHED_PROPOSALS = 2;
 
 export class GetAgoraProposalsCommand extends Command<Payload, Response> {
   public readonly name = 'GetAgoraProposalsCommand' as const;
@@ -33,7 +34,7 @@ export class GetAgoraProposalsCommand extends Command<Payload, Response> {
   async handle() {
     try {
       const response = await fetch(
-        `${AGORA_API_URL}?limit=2&offset=${this.payload.offset}`,
+        `${AGORA_API_URL}?limit=${NUMBER_OF_FETCHED_PROPOSALS}&offset=${this.payload.offset}`,
         {
           headers: {
             'Content-Type': 'application/json',
