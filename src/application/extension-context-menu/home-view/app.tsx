0d9a217501ce49f3dbfea3bc21fdbbad0bcba9f3
Daniel Chutkowski <daniel.chutkowski@gmail.com>
2024-09-05 19:10:15 +0200

    added clear input button

--- src/application/extension-context-menu/home-view/app.tsx
+++ src/application/extension-context-menu/home-view/app.tsx
@@ -52,8 +52,8 @@ export const App = () => {
           Look up your wallet address
         </label>
         <input
+          type="search"
           autoFocus
-          type="text"
           id="first_name"
           className="mb-1.5 box-border block h-[38px] w-full rounded border border-[#cccccc] bg-white px-3 py-2 align-middle font-sans text-sm leading-[1.428571429] text-[#333333] outline-none"
           placeholder="hello@idriss.xyz  |  +1 650...  |  @IDriss_xyz"
