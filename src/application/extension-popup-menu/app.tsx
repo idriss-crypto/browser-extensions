c6b76778bfffdff1918151a37637a09a3329b40f
Daniel Chutkowski <daniel.chutkowski@gmail.com>
2024-09-16 20:44:31 +0200

    removed unused exports

--- src/application/extension-popup-menu/app.tsx
+++ src/application/extension-popup-menu/app.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import { useExtensionSettings } from 'shared/extension';
 import { Closable } from 'shared/ui';
