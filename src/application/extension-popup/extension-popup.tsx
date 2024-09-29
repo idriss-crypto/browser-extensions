9e5c439bb30bd8986ac2d08b8b3ad47f86c81242
Daniel Chutkowski <daniel.chutkowski@gmail.com>
2024-09-29 16:33:50 +0200

    changed the scrollbar color

--- src/application/extension-popup/extension-popup.tsx
+++ src/application/extension-popup/extension-popup.tsx
@@ -21,7 +21,7 @@ export const ExtensionPopup = () => {
       className="fixed right-2 top-2 z-[999999] flex h-[512px] w-[470px] flex-col overflow-hidden rounded-md bg-white p-0 shadow-lg"
     >
       <TopBar />
-      <div className="grow overflow-y-auto bg-gray-100 [scrollbar-color:gray_#b7b7b7] [scrollbar-width:thin]">
+      <div className="grow overflow-y-auto bg-gray-100 [scrollbar-color:gray_transparent] [scrollbar-width:thin]">
         <Routes>
           <Route path={EXTENSION_POPUP_ROUTE.HOME} element={<HomeView />} />
           <Route
