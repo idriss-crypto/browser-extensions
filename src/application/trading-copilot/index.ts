b70e54f0c1e5dc91d17dddf1bf1b73a53e85d672
Daniel Chutkowski <daniel.chutkowski@gmail.com>
2024-10-15 18:10:04 +0200

    lint

--- src/application/trading-copilot/index.ts
+++ src/application/trading-copilot/index.ts
@@ -1,4 +1,2 @@
 export { SubscriptionsManagement } from './subscriptions-management';
-export {
-  COMMAND_MAP as TRADING_COPILOT_COMMAND_MAP,
-} from './commands';
+export { COMMAND_MAP as TRADING_COPILOT_COMMAND_MAP } from './commands';
