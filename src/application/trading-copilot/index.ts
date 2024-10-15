acb6febbf80e70119344a289e5e93491427f0e08
Daniel Chutkowski <daniel.chutkowski@gmail.com>
2024-10-15 18:08:23 +0200

    removed unused exports

--- src/application/trading-copilot/index.ts
+++ src/application/trading-copilot/index.ts
@@ -1,10 +1,4 @@
 export { SubscriptionsManagement } from './subscriptions-management';
 export {
-  AddTradingCopilotSubscriptionCommand,
-  GetEnsAddressCommand,
-  GetEnsInfoCommand,
-  GetTradingCopilotSubscriptionsCommand,
-  RemoveTradingCopilotSubscriptionCommand,
   COMMAND_MAP as TRADING_COPILOT_COMMAND_MAP,
 } from './commands';
-export type { Subscription } from './types';
