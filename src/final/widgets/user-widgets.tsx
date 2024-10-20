1da44fd3661a17c738ee54c7a526dbe5223a12dd
nemmtor <kacper0witas@gmail.com>
2024-10-20 18:58:18 +0200

    fix: delay rendering user widgets by 3s so there is no flickering

--- src/final/widgets/user-widgets.tsx
+++ src/final/widgets/user-widgets.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import { ErrorBoundary } from 'shared/observability';
 import { GitcoinDonationWidget } from 'application/gitcoin';
 import { IdrissSendWidget } from 'application/idriss-send';
@@ -7,10 +9,22 @@ import { useUserWidgets } from '../hooks';
 export const UserWidgets = () => {
   const { widgets } = useUserWidgets();
 
+  const [canRender, setCanRender] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCanRender(true);
+    }, 3000);
+  }, []);
+
+  if (!canRender) {
+    return null;
+  }
+
   return (
     <ErrorBoundary>
       {widgets.map((widget) => {
-        const key = `${widget.username}-${widget.top}`;
+        const key = `${widget.username}-${Math.floor(widget.top)}`;
 
         if (widget.type === 'gitcoin') {
           return <GitcoinDonationWidget key={key} widgetData={widget} />;
