0592351e5d7bc045fd46ba0bc3f9bd18ed9fa715
nemmtor <50832972+nemmtor@users.noreply.github.com>
2024-10-21 23:11:51 +0200

    fix: storybook (#92)

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,31 +1,14 @@
 import React from 'react';
-import NiceModal from '@ebay/nice-modal-react';
 import type { Preview } from '@storybook/react';
-
-import { PortalProvider, QueryProvider } from '../src/shared/ui';
-import { WithObservabilityScope } from '../src/shared/observability';
-import { WalletContextProvider } from '../src/shared/web3';
-import { ExtensionSettingsProvider } from '../src/shared/extension';
-
-import './tailwind.css';
+import { Providers } from '../src/infrastructure/application/providers';
 
 const preview: Preview = {
   decorators: [
     (Story) => {
       return (
-        <WithObservabilityScope>
-          <PortalProvider>
-            <QueryProvider>
-              <NiceModal.Provider>
-                <WalletContextProvider disabledWalletsRdns={[]}>
-                  <ExtensionSettingsProvider>
-                    <Story />
-                  </ExtensionSettingsProvider>
-                </WalletContextProvider>
-              </NiceModal.Provider>
-            </QueryProvider>
-          </PortalProvider>
-        </WithObservabilityScope>
+        <Providers>
+          <Story />
+        </Providers>
       );
     },
   ],
