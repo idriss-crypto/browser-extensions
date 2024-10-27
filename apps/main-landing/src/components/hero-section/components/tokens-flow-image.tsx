4978e6e84bc4d66d6a8738780e018b97238d1d7a
Daniel Chutkowski <daniel.chutkowski@gmail.com>
2024-10-27 12:41:53 +0100

    added md margin to tokens flow

--- apps/main-landing/src/components/hero-section/components/tokens-flow-image.tsx
+++ apps/main-landing/src/components/hero-section/components/tokens-flow-image.tsx
@@ -2,7 +2,7 @@ export const TokensFlowImage = () => {
   return (
       <img
         src="/tokens-flow.png"
-        className="min-w-[500px] -mt-[22%] lg:-mt-[15%] w-full z-0"
+        className="min-w-[500px] -mt-[22%] md:-mt-[18%] lg:-mt-[15%] w-full z-0"
       />
   );
 };
