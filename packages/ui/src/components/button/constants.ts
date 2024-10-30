47364f2e46caadc4adbbc20a33b9267ba29310e5
nemmtor <50832972+nemmtor@users.noreply.github.com>
2024-10-30 19:34:53 +0100

    feat: typography 2 (#109)

--- packages/ui/src/components/button/constants.ts
+++ packages/ui/src/components/button/constants.ts
@@ -1,7 +1,7 @@
 import { ButtonVariants } from './variants';
 
 export const BUTTON_SIZE_TO_ICON_SIZE: Record<ButtonVariants['size'], number> =
-{
-  large: 20,
-  medium: 20,
-};
+  {
+    large: 20,
+    medium: 20,
+  };
