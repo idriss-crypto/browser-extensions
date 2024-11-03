0553857f7eb7d8e5f70dc9ba856a2249b8178776
Daniel Chutkowski <daniel.chutkowski@gmail.com>
2024-11-03 19:01:53 +0100

    removed unused exports

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -3,17 +3,5 @@ export {
   Dialog,
   IconButton,
   VisuallyHidden,
-  DiscordSocialIcon,
-  FarcasterSocialIcon,
-  XSocialIcon,
-  InstagramSocialIcon,
-  TiktokSocialIcon,
-  YoutubeSocialIcon,
-  AavegotchiLogo,
-  AcrossLogo,
-  GitcoinLogo,
-  ParallelLogo,
-  PolymarketLogo,
-  TallyLogo,
 } from './components';
 export { classes } from './utils';
