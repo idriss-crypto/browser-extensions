277f2144505219a212885cee486dbef124980910
nemmtor <kacper0witas@gmail.com>
2024-11-03 14:57:33 +0100

    fix: make navbar pos absolute

--- apps/main-landing/src/components/top-bar/top-bar.tsx
+++ apps/main-landing/src/components/top-bar/top-bar.tsx
@@ -5,7 +5,7 @@ import { Navigation } from './components';
 
 export const TopBar = () => {
   return (
-    <div className="container flex items-center justify-between py-1 lg:py-3">
+    <div className="container absolute inset-x-0 top-0 z-1 flex items-center justify-between py-1 lg:py-3">
       <Link href="/">
         <Image
           src="/logo-idriss-dark.png"
