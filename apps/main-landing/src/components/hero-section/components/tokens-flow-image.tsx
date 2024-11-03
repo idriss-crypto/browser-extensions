ff32f34e6df306d2f3c7001a4e753060adb4a355
Daniel Chutkowski <daniel.chutkowski@gmail.com>
2024-11-03 18:55:51 +0100

    removed unused components

--- apps/main-landing/src/components/hero-section/components/tokens-flow-image.tsx
+++
@@ -1,8 +0,0 @@
-export const TokensFlowImage = () => {
-  return (
-    <img
-      src="/tokens-flow.png"
-      className="z-0 -mt-[22%] w-full min-w-[500px] md:-mt-[18%] lg:-mt-[15%]"
-    />
-  );
-};
