76fcacd7039e7d6ddd451ee30ff5e88b2983eb5d
nemmtor <kacper0witas@gmail.com>
2024-11-08 23:04:00 +0100

    feat: remove basic auth

--- apps/main-landing/src/app/layout.tsx
+++ apps/main-landing/src/app/layout.tsx
@@ -26,48 +26,47 @@ const aeonikPro = localFont({
   variable: '--font-aeonikpro',
 });
 
-// const DEPLOYMENT_URL = process.env.RAILWAY_PUBLIC_DOMAIN
-//   ? new URL(`https://${process.env.RAILWAY_PUBLIC_DOMAIN}`)
-//   : undefined;
+const DEPLOYMENT_URL = process.env.RAILWAY_PUBLIC_DOMAIN
+  ? new URL(`https://${process.env.RAILWAY_PUBLIC_DOMAIN}`)
+  : undefined;
 
 // ts-unused-exports:disable-next-line
 export const metadata: Metadata = {
   title: 'Superpowers for your internet',
   description:
     'Our apps bring the power of crypto and AI to your browsing experience, empower creators through digital ownership, and help find what’s true on the internet.',
-  // TODO: uncomment before final release
-  // icons: [
-  //   {
-  //     rel: 'apple-touch-icon',
-  //     sizes: '180x180',
-  //     url: '/apple-touch-icon.png',
-  //   },
-  //   {
-  //     rel: 'icon',
-  //     sizes: '32x32',
-  //     type: 'image/png',
-  //     url: '/favicon-32x32.png',
-  //   },
-  //   {
-  //     rel: 'icon',
-  //     sizes: '16x16',
-  //     type: 'image/png',
-  //     url: '/favicon-16x16.png',
-  //   },
-  // ],
-  // metadataBase: DEPLOYMENT_URL,
-  // openGraph: {
-  //   type: 'website',
-  //   url: DEPLOYMENT_URL,
-  //   title: 'Superpowers for your internet',
-  //   description:
-  //     'Our apps bring the power of crypto and AI to your browsing experience, empower creators through digital ownership, and help find what’s true on the internet.',
-  //   images: [
-  //     {
-  //       url: '/og.png',
-  //     },
-  //   ],
-  // },
+  icons: [
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      url: '/apple-touch-icon.png',
+    },
+    {
+      rel: 'icon',
+      sizes: '32x32',
+      type: 'image/png',
+      url: '/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      sizes: '16x16',
+      type: 'image/png',
+      url: '/favicon-16x16.png',
+    },
+  ],
+  metadataBase: DEPLOYMENT_URL,
+  openGraph: {
+    type: 'website',
+    url: DEPLOYMENT_URL,
+    title: 'Superpowers for your internet',
+    description:
+      'Our apps bring the power of crypto and AI to your browsing experience, empower creators through digital ownership, and help find what’s true on the internet.',
+    images: [
+      {
+        url: '/og.png',
+      },
+    ],
+  },
 };
 
 // ts-unused-exports:disable-next-line
