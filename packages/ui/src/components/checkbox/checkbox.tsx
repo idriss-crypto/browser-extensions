e0bc15b9eab3eaebe4ea36e7282f5145613b864c
SolutionsEngineer <106707979+SolutionsEngineer@users.noreply.github.com>
2024-11-10 04:03:58 +0100

    Feat/checkbox (#133)

---
+++ packages/ui/src/components/checkbox/checkbox.tsx
@@ -0,0 +1,38 @@
+import * as RadixCheckbox from '@radix-ui/react-checkbox';
+import { forwardRef } from 'react';
+
+import { CheckIcon } from './assets';
+
+type Properties = {
+  label?: string;
+  value: boolean;
+  onChange: (value: boolean) => void;
+};
+
+export const Checkbox = forwardRef<HTMLButtonElement, Properties>(
+  ({ label, value, onChange }, reference) => {
+    return (
+      <label>
+        <div className="flex items-center">
+          <RadixCheckbox.Root
+            ref={reference}
+            className="flex size-5 items-center justify-center rounded border-2 border-neutral-400 hover:border-mint-500 focus:border-mint-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 active:border-mint-600 disabled:border-neutral-200 data-[state=checked]:border-none data-[state=checked]:bg-mint-400"
+            checked={value}
+            onCheckedChange={onChange}
+          >
+            <RadixCheckbox.Indicator className="text-mint-900">
+              <CheckIcon />
+            </RadixCheckbox.Indicator>
+          </RadixCheckbox.Root>
+          {label && (
+            <span className="pl-2 text-body6 text-neutralGreen-900 disabled:text-neutral-500 lg:text-body5">
+              {label}
+            </span>
+          )}
+        </div>
+      </label>
+    );
+  },
+);
+
+Checkbox.displayName = 'Checkbox';
