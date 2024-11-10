e2338c5a4223dfc5eb75ff346d75ffa64a122437
Daniel Chutkowski <daniel.chutkowski@gmail.com>
2024-11-10 03:56:22 +0100

    wrapped checkbox with label

--- packages/ui/src/components/checkbox/checkbox.tsx
+++ packages/ui/src/components/checkbox/checkbox.tsx
@@ -9,36 +9,30 @@ type Properties = {
   onChange: (value: boolean) => void;
 };
 
-const Base = forwardRef<HTMLButtonElement, Properties>(
+export const Checkbox = forwardRef<HTMLButtonElement, Properties>(
   ({ label, value, onChange }, reference) => {
     return (
-      <form>
+      <label>
         <div className="flex items-center">
           <RadixCheckbox.Root
             ref={reference}
-            className="flex size-5 appearance-none items-center justify-center rounded border-2 border-neutral-400 hover:border-mint-500 focus:border-mint-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 active:border-mint-600 disabled:border-neutral-200 data-[state=checked]:border-none data-[state=checked]:bg-mint-400"
+            className="flex size-5 items-center justify-center rounded border-2 border-neutral-400 hover:border-mint-500 focus:border-mint-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 active:border-mint-600 disabled:border-neutral-200 data-[state=checked]:border-none data-[state=checked]:bg-mint-400"
             checked={value}
             onCheckedChange={onChange}
-            id="_checkbox"
           >
             <RadixCheckbox.Indicator className="text-mint-900">
               <CheckIcon />
             </RadixCheckbox.Indicator>
           </RadixCheckbox.Root>
           {label && (
-            <label
-              className="pl-2 text-body5 text-neutralGreen-900 disabled:text-neutral-500"
-              htmlFor="_checkbox"
-            >
+            <span className="pl-2 text-body6 text-neutralGreen-900 disabled:text-neutral-500 lg:text-body5">
               {label}
-            </label>
+            </span>
           )}
         </div>
-      </form>
+      </label>
     );
   },
 );
 
-export const Checkbox = Object.assign(Base, {
-  displayName: 'Checkbox',
-});
+Checkbox.displayName = 'Checkbox';
