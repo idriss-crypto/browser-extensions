714e4a4f5bf7177606092fd43f1cf366a2fa2261
lennardevertz <levertz@idriss.xyz>
2024-11-11 17:48:56 +0100

    - update imports

--- apps/main-landing/next.config.ts
+++ apps/main-landing/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next';
 import withBundleAnalyzer from '@next/bundle-analyzer';
 
-import { LEGACY_URLS } from '@/constants';
+import { LEGACY_URLS } from './src/constants';
 
 const nextConfig: NextConfig = {
   productionBrowserSourceMaps: true,
