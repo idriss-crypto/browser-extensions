eff8b526c70136025e690d731777b44c751e86c6
Daniel Chutkowski <daniel.chutkowski@gmail.com>
2024-11-11 20:07:24 +0100

    changed products section navigation to be sticky on mobile

--- apps/main-landing/src/components/products-section/components/product-section.tsx
+++ apps/main-landing/src/components/products-section/components/product-section.tsx
@@ -60,7 +60,7 @@ export const ProductSection = ({
   return (
     <div className={classes('relative flex size-full bg-mint-100', className)}>
       <div
-        className="flex size-full flex-col bg-[linear-gradient(114deg,_#022B1E_34.81%,_#079165_123.57%)] px-4 py-10 transition-[border-radius] duration-150 will-change-[border-radius] lg:px-[120px] lg:py-[80px] [@media(max-height:1100px)]:py-[30px]"
+        className="flex size-full flex-col bg-[linear-gradient(114deg,_#022B1E_34.81%,_#079165_123.57%)] px-4 transition-[border-radius] duration-150 will-change-[border-radius] lg:px-[120px] lg:pt-[80px] lg:[@media(max-height:1100px)]:pt-[30px]"
         style={style}
       >
         <div className="container flex flex-col gap-10 p-0 lg:gap-[104px] lg:[@media(max-height:1000px)]:gap-[30px] lg:[@media(max-height:770px)]:gap-[24px] lg:[@media(min-height:1001px)]:[@media(max-height:1100px)]:gap-[50px]">
@@ -118,30 +118,30 @@ export const ProductSection = ({
                 className="bottom-0 right-0 top-1/2 z-0 lg:absolute lg:max-w-[45%] lg:-translate-y-1/2 lg:[@media(max-width:1440px)]:[@media(max-height:1000px)]:translate-y-[-30%] lg:[@media(min-height:1300px)]:-translate-y-full [@media(min-width:1001px)]:[@media(min-height:901px)]:[@media(max-height:1100px)]:translate-y-[-40%]"
               />
             )}
-          </div>
-          <div className="z-1 overflow-hidden">
-            <div
-              className={classes(
-                'grid size-fit flex-wrap items-start gap-6 p-1.5 transition-transform duration-1000 md:grid-cols-2 lg:grid-cols-2',
-                fadeOut && 'translate-y-[-120%]',
-              )}
-            >
-              {features.map((feature) => {
-                return (
-                  <ProductInfo
-                    key={feature.title}
-                    icon={
-                      <Icon
-                        name={feature.icon}
-                        size={65}
-                        className="size-10 text-[#55EB3C] lg:size-[65px]"
-                      />
-                    }
-                    title={feature.title}
-                    description={feature.description}
-                  />
-                );
-              })}
+            <div className="z-1 overflow-hidden">
+              <div
+                className={classes(
+                  'grid size-fit flex-wrap items-start gap-6 p-1.5 pb-10 transition-transform duration-1000 md:grid-cols-2 lg:grid-cols-2 lg:pb-[80px] [@media(max-height:1100px)]:pb-[30px]',
+                  fadeOut && 'translate-y-[-120%]',
+                )}
+              >
+                {features.map((feature) => {
+                  return (
+                    <ProductInfo
+                      key={feature.title}
+                      icon={
+                        <Icon
+                          name={feature.icon}
+                          size={65}
+                          className="size-10 text-[#55EB3C] lg:size-[65px]"
+                        />
+                      }
+                      title={feature.title}
+                      description={feature.description}
+                    />
+                  );
+                })}
+              </div>
             </div>
           </div>
         </div>
