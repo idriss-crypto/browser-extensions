5b5a5d49c0f370ab848311c1cbcee27d20db949c
SolutionsEngineer <106707979+SolutionsEngineer@users.noreply.github.com>
2024-11-12 02:23:43 +0100

    added check if page is rendered in marquee (#152)

--- packages/ui/src/components/marquee/marquee.tsx
+++ packages/ui/src/components/marquee/marquee.tsx
@@ -1,4 +1,5 @@
-import { ReactNode } from 'react';
+'use client';
+import { ReactNode, useEffect, useState } from 'react';
 
 import { classes } from '../../utils';
 
@@ -13,7 +14,13 @@ export const Marquee = ({
   pauseOnHover = true,
   className,
 }: Properties) => {
+  const [isRendered, setIsRendered] = useState(false);
+
   const spaceClassName = 'space-x-10 lg:space-x-[60px]';
+
+  useEffect(() => {
+    setIsRendered(true);
+  }, []);
 
   return (
     <div
@@ -24,8 +31,9 @@ export const Marquee = ({
     >
       <ul
         className={classes(
-          'flex animate-marquee list-none whitespace-nowrap pl-10 lg:pl-[60px]',
+          'flex list-none whitespace-nowrap pl-10 will-change-[transform] lg:pl-[60px]',
           spaceClassName,
+          isRendered && 'animate-marquee',
           pauseOnHover &&
             'group-focus-within:paused-animation group-hover:paused-animation',
         )}
@@ -44,8 +52,9 @@ export const Marquee = ({
 
       <ul
         className={classes(
-          'absolute flex animate-marquee2 list-none whitespace-nowrap pl-10 lg:pl-[60px]',
+          'absolute flex list-none whitespace-nowrap pl-10 will-change-[transform] lg:pl-[60px]',
           spaceClassName,
+          isRendered && 'animate-marquee2',
           pauseOnHover &&
             'group-focus-within:paused-animation group-hover:paused-animation',
         )}
