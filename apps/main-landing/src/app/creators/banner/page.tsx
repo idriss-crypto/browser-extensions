5f6b8a8c6abdcb890343ef7b62391368ddbdd829
Daniel Chutkowski <daniel.chutkowski@gmail.com>
2024-11-21 22:33:44 +0100

    format

--- apps/main-landing/src/app/creators/banner/page.tsx
+++ apps/main-landing/src/app/creators/banner/page.tsx
@@ -5,6 +5,7 @@ import { IconButton } from '@idriss-xyz/ui/icon-button';
 import { useState } from 'react';
 import { Icon } from '@idriss-xyz/ui/icon';
 import { classes } from '@idriss-xyz/ui/utils';
+import { ANNOUNCEMENT_LINK } from '@idriss-xyz/constants';
 
 import { TopBar } from '@/components';
 import { backgroundLines2, backgroundLines3 } from '@/assets';
@@ -25,7 +26,6 @@ import {
   banner8,
   banner9,
 } from './assets';
-import { ANNOUNCEMENT_LINK } from '@idriss-xyz/constants';
 
 const banners = [
   banner1,
@@ -61,7 +61,7 @@ export default function Banner() {
   return (
     <Providers>
       <TopBar />
-      <main className="relative flex min-h-screen grow lg:flex-row flex-col items-center lg:items-start lg:justify-center justify-around gap-4 overflow-hidden bg-[radial-gradient(111.94%_122.93%_at_16.62%_0%,_#E7F5E7_0%,_#b5d8ae_100%)] px-2 pb-1 pt-[56px] lg:px-0">
+      <main className="relative flex min-h-screen grow flex-col items-center justify-around gap-4 overflow-hidden bg-[radial-gradient(111.94%_122.93%_at_16.62%_0%,_#E7F5E7_0%,_#b5d8ae_100%)] px-2 pb-1 pt-[56px] lg:flex-row lg:items-start lg:justify-center lg:px-0">
         <Image
           priority
           src={backgroundLines2}
