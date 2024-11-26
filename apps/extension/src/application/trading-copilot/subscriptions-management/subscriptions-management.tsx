58c288e952f6060f9246bd201d31383e264401bd
Daniel Chutkowski <daniel.chutkowski@gmail.com>
2024-11-27 00:09:03 +0100

    removed test component

--- apps/extension/src/application/trading-copilot/subscriptions-management/subscriptions-management.tsx
+++ apps/extension/src/application/trading-copilot/subscriptions-management/subscriptions-management.tsx
@@ -1,7 +1,4 @@
-import { useState } from 'react';
-
 import { useCommandMutation, useCommandQuery } from 'shared/messaging';
-import { Button, LazyImage, useNotification } from 'shared/ui';
 
 import {
   AddTradingCopilotSubscriptionCommand,
@@ -12,29 +9,7 @@ import { Subscription } from '../types';
 
 import { SubscriptionForm, SubscriptionsList } from './components';
 
-const TestComponent = () => {
-  const [clicked, setClicked] = useState(false);
-
-  return (
-    <div>
-      <h1>Hey, you</h1>
-      <h3>I have a pleasure to notify you about a new feature</h3>
-      <LazyImage src="https://www.lego.com/cdn/cs/set/assets/blt42e887277e9ae38b/60399_alt1.png?fit=bounds&format=png&width=1500&height=1500&dpr=1" />
-      <Button
-        onClick={() => {
-          setClicked(true);
-          console.log('clicked 🎉🎉');
-        }}
-      >
-        {clicked ? 'Click to see more' : 'More coming soon!'}
-      </Button>
-    </div>
-  );
-};
-
 export const SubscriptionsManagement = () => {
-  const notification = useNotification();
-
   const subscriptionsQuery = useCommandQuery({
     command: new GetTradingCopilotSubscriptionsCommand({}),
   });
@@ -47,7 +22,6 @@ export const SubscriptionsManagement = () => {
   const handleAddNewSubscription = async (subscription: Subscription) => {
     await subscribe.mutateAsync({ subscription });
     void subscriptionsQuery.refetch();
-    notification.show(<TestComponent />, 'bottom-right');
   };
 
   const handleUnsubscribe = async (ensName: Subscription['ensName']) => {
