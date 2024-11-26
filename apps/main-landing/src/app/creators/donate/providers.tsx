154053669bf3ce2ccb68201b2280db4410624ea4
Daniel <daniel0ar@hotmail.com>
2024-11-26 10:20:32 +0700

    fix: Modify supported networks, add custom networks

--- apps/main-landing/src/app/creators/donate/providers.tsx
+++ apps/main-landing/src/app/creators/donate/providers.tsx
@@ -12,21 +12,42 @@ import {
   mainnet,
   polygon,
   optimism,
-  arbitrum,
+  mantle,
   base,
 } from 'wagmi/chains';
+import { defineChain } from 'viem';
 
 import { QueryProvider } from '@/providers';
+import { ALEPH_LOGO } from './logos';
 
 type Properties = {
   children: ReactNode;
 };
 
 // TODO: Check correct place for this config
+
+export const alephzero = defineChain({
+  id: 41_455,
+  name: 'Aleph Zero EVM',
+  logo: ALEPH_LOGO,
+  nativeCurrency: {
+    name: 'AZERO',
+    symbol: 'AZERO',
+    decimals: 18,
+  },
+  rpcUrls: { default: { http: ['https://rpc.alephzero.raas.gelato.cloud'] } },
+  blockExplorers: {
+    default: {
+      name: 'Evm Explorer',
+      url: 'https://evm-explorer.alephzero.org',
+    },
+  },
+})
+
 const config = getDefaultConfig({
   appName: 'IDRISS Creators Login',
-  projectId: 'YOUR_PROJECT_ID', // TODO: change for WalletConnect ID
-  chains: [mainnet, polygon, optimism, arbitrum, base],
+  projectId: 'd9b05a544ca612833e810a77724e5af8', // TODO: change for WalletConnect ID
+  chains: [mainnet, polygon, optimism, mantle, base],
   ssr: true,
 });
 
