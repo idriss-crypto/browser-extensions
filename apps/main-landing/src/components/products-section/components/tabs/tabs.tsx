d8d5aa41698de0f7183f33160cd2439f08f7a4ad
Mateusz Stawski <mateuszmstawski@gmail.com>
2024-12-07 15:51:14 +0100

    refactor: remove empty string in code

--- apps/main-landing/src/components/products-section/components/tabs/tabs.tsx
+++ apps/main-landing/src/components/products-section/components/tabs/tabs.tsx
@@ -43,7 +43,7 @@ export const Tabs = ({ activeOptionKey, options, asLink, ref }: Properties) => {
               {option.name}
             </span>
           );
-        })}{' '}
+        })}
       </div>
     </div>
   );
