db9852af920dd9d70299757081e61feaeb54ae08
Maciej Kutynia <kutynia.maciej@gmail.com>
2024-12-07 19:24:53 +0100

    fix format

--- apps/main-landing/src/app/creators/donate/hooks/use-sender.ts
+++ apps/main-landing/src/app/creators/donate/hooks/use-sender.ts
@@ -70,7 +70,7 @@ export const useSender = ({ walletClient, publicClient }: Properties) => {
         walletClient,
       });
 
-      if (isNativeTokenAddress(sendPayload.tokenAddress as `0x${string}`)) {
+      if (isNativeTokenAddress(sendPayload.tokenAddress)) {
         nativeTransaction.mutate({
           tokensToSend,
           recipientAddress,
@@ -82,7 +82,7 @@ export const useSender = ({ walletClient, publicClient }: Properties) => {
       } else {
         erc20Transaction.mutate({
           recipientAddress,
-          tokenAddress: sendPayload.tokenAddress as `0x${string}`,
+          tokenAddress: sendPayload.tokenAddress,
           walletClient,
           publicClient,
           tokensToSend,
