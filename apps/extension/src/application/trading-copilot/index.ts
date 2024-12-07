a59d4ed12f305dad67e75ce096fc24ead30fdf55
Mateusz Stawski <mateuszmstawski@gmail.com>
2024-12-07 14:48:07 +0100

    refactor: code cleanup after merge

--- apps/extension/src/application/trading-copilot/index.ts
+++ apps/extension/src/application/trading-copilot/index.ts
@@ -1,6 +1,9 @@
 import { SubscriptionsManagement } from './subscriptions-management';
 // TODO: this is only due to unused-exports check, will be resolved once we implement copilot feature
 JSON.stringify(typeof SubscriptionsManagement);
-export { COMMAND_MAP as TRADING_COPILOT_COMMAND_MAP, GetEnsInfoCommand } from './commands';
+export {
+  COMMAND_MAP as TRADING_COPILOT_COMMAND_MAP,
+  GetEnsInfoCommand,
+} from './commands';
 export type { SwapData } from './types';
 export { SubscriptionsManagement } from './subscriptions-management';
