db92839640c5df22d4df346ed9568d64881cae2b
Mateusz Stawski <mateuszmstawski@gmail.com>
2024-12-08 23:54:14 +0100

    fix: revert wrong merge changes

--- apps/extension/src/application/trading-copilot/subscriptions-management/components/subscriptions-list/subscription-item.tsx
+++ apps/extension/src/application/trading-copilot/subscriptions-management/components/subscriptions-list/subscription-item.tsx
@@ -1,8 +1,10 @@
 import { useCallback } from 'react';
 import { ExternalLink } from '@idriss-xyz/ui/external-link';
+import { Icon as IdrissIcon } from '@idriss-xyz/ui/icon';
+import { IconButton } from '@idriss-xyz/ui/icon-button';
 
 import { useCommandQuery } from 'shared/messaging';
-import { Icon, IconButton, LazyImage, getGithubUserLink } from 'shared/ui';
+import { Icon, LazyImage, getGithubUserLink } from 'shared/ui';
 import { getTwitterUserLink } from 'host/twitter';
 import { GetEnsNameCommand } from 'application/trading-copilot/commands/get-ens-name';
 
@@ -96,9 +98,15 @@ const SubscriptionItemContent = ({
       <div className="flex items-center">
         <LazyImage
           src={avatarQuery.data}
-          className="size-5 rounded-full"
+          className="size-8 rounded-full border border-neutral-400 bg-neutral-200"
           fallbackComponent={
-            <Icon size={20} name="PersonIcon" className="rounded-full" />
+            <div className="flex size-8 items-center justify-center rounded-full border border-neutral-300 bg-neutral-200">
+              <IdrissIcon
+                size={20}
+                name="CircleUserRound"
+                className="text-neutral-500"
+              />
+            </div>
           }
         />
 
@@ -110,31 +118,45 @@ const SubscriptionItemContent = ({
               <Icon
                 size={16}
                 name="TwitterLogoIcon"
-                className="[&>path]:fill-rule-non-zero"
+                className="text-[#757575] [&>path]:fill-rule-non-zero"
               />
             </ExternalLink>
           )}
           {githubQuery.data && (
             <ExternalLink href={getGithubUserLink(githubQuery.data)}>
-              <Icon size={16} name="GitHubLogoIcon" />
+              <Icon
+                size={16}
+                name="GitHubLogoIcon"
+                className="text-[#757575]"
+              />
             </ExternalLink>
           )}
           {discordQuery.data && (
             <span title={discordQuery.data}>
-              <Icon size={16} name="DiscordLogoIcon" />
+              <Icon
+                size={16}
+                name="DiscordLogoIcon"
+                className="text-[#757575]"
+              />
             </span>
           )}
           {emailQuery.data && (
             <ExternalLink href={`mailto:${emailQuery.data}`}>
-              <Icon size={16} name="EnvelopeClosedIcon" />
+              <Icon
+                size={16}
+                name="EnvelopeClosedIcon"
+                className="text-[#757575]"
+              />
             </ExternalLink>
           )}
         </p>
       </div>
       <IconButton
-        iconProps={{ name: 'Cross1Icon' }}
-        className="text-red-700"
+        intent="tertiary"
+        size="small"
+        iconName="Trash2"
         onClick={remove}
+        className="text-red-500"
       />
     </li>
   );
