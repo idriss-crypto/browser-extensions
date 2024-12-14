2654567bfd8b1afe61210e45bf9ae042d0ebb254
Daniel Chutkowski <daniel.chutkowski@gmail.com>
2024-12-14 17:03:42 +0100

    format fix

--- apps/main-landing/src/components/top-bar/top-bar.tsx
+++ apps/main-landing/src/components/top-bar/top-bar.tsx
@@ -7,12 +7,7 @@ export const TopBar = () => {
     <div className="absolute inset-x-0 top-0 z-topBar w-full px-safe">
       <div className="container flex items-center justify-between py-1 lg:py-3">
         <Link href="/">
-          <img
-            src="/idriss-dark-logo.svg"
-            height={24}
-            width={98}
-            alt=""
-          />
+          <img src="/idriss-dark-logo.svg" height={24} width={98} alt="" />
         </Link>
         <Navigation />
       </div>
