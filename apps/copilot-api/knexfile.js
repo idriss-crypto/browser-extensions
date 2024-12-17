b7519ce5d47f0082ad65294473cbaab62684704b
Maciej Kutynia <kutynia.maciej@gmail.com>
2024-12-17 22:42:45 +0100

    fix challengeMessage

--- apps/copilot-api/knexfile.js
+++ apps/copilot-api/knexfile.js
@@ -1,20 +1,18 @@
-// knexfile.js
-
-require("dotenv").config();
+require('dotenv').config();
 
 module.exports = {
-    development: {
-        client: "pg",
-        connection: process.env.DATABASE_URL,
-        migrations: {
-            directory: "./migrations",
-        },
+  development: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './migrations',
     },
-    production: {
-        client: "pg",
-        connection: process.env.DATABASE_URL,
-        migrations: {
-            directory: "./migrations",
-        },
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './migrations',
     },
+  },
 };
--- apps/copilot-api/routes/auth.ts
+++ apps/copilot-api/routes/auth.ts
@@ -1,112 +1,117 @@
-import express from 'express'
-import type {Request, Response} from 'express'
-import dotenv from "dotenv";
-import jwt from "jsonwebtoken";
-import {throwInternalError} from "../middleware/error.middleware";
-import {isAddress, verifyMessage} from "viem";
-import {dataSource} from "../db";
-import {AddressesEntity} from "../entities/addreesses.entity";
-import {UsersEntity} from "../entities/users.entity";
-import {v4 as uuidv4} from 'uuid'
+import express from 'express';
+import type { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+import { throwInternalError } from '../middleware/error.middleware';
+import { isAddress, verifyMessage } from 'viem';
+import { dataSource } from '../db';
+import { AddressesEntity } from '../entities/addreesses.entity';
+import { UsersEntity } from '../entities/users.entity';
+import { v4 as uuidv4 } from 'uuid';
 
 dotenv.config();
 
 const router = express.Router();
 
-
-const addressesRepo = dataSource.getRepository(AddressesEntity)
-const usersRepo = dataSource.getRepository(UsersEntity)
+const addressesRepo = dataSource.getRepository(AddressesEntity);
+const usersRepo = dataSource.getRepository(UsersEntity);
 
 router.post('/login', async (req: Request, res: Response) => {
-  const {signature, walletAddress, challengeMessage} = req.body;
+  const { signature, walletAddress, challengeMessage } = req.body;
 
   if (!isAddress(walletAddress)) {
-    res.status(403).json({error: "Invalid wallet address"})
-    return
+    res.status(403).json({ error: 'Invalid wallet address' });
+    return;
   }
 
   try {
-    const address = await verifyMessage({
+    const valid_address = await verifyMessage({
       message: challengeMessage,
       address: walletAddress,
-      signature
+      signature,
     });
 
-    if (!address) {
-      res.status(403).json({error: "Invalid signature"})
-      return
+    if (!valid_address) {
+      res.status(403).json({ error: 'Invalid signature' });
+      return;
     }
 
+    const existingAddress = await addressesRepo.findOne({
+      where: { address: walletAddress },
+    });
 
-    const existingAddress = await addressesRepo.findOne({where: {address: walletAddress}})
-
-    const user = await usersRepo.findOne({where: {uuid: existingAddress?.userId}})
-
+    const user = await usersRepo.findOne({
+      where: { uuid: existingAddress?.userId },
+    });
 
     if (!user) {
-      const newUser = await usersRepo.save({})
+      const newUser = await usersRepo.save({});
       const payload = {
         user: {
-          id: newUser.uuid
-        }
-      }
+          id: newUser.uuid,
+        },
+      };
 
-      const token = jwt.sign(payload, process.env.JWT_SECRET as string, {expiresIn: '7d'})
+      const token = jwt.sign(payload, process.env.JWT_SECRET as string, {
+        expiresIn: '7d',
+      });
 
-      res.status(200).send({token})
+      res.status(200).send({ token });
       return;
     }
 
     const payload = {
       user: {
         id: user.uuid,
-      }
-    }
+      },
+    };
 
-    const token = jwt.sign(payload, process.env.JWT_SECRET as string, {expiresIn: '7d'})
+    const token = jwt.sign(payload, process.env.JWT_SECRET as string, {
+      expiresIn: '7d',
+    });
 
-    res.status(200).send({token})
-  } catch (err) {
-
-  }
-})
+    res.status(200).send({ token });
+  } catch (err) {}
+});
 
 router.post('/wallet-address', async (req, res) => {
   try {
-    const {walletAddress} = req.body
+    const { walletAddress } = req.body;
     const nonce = uuidv4();
     const timestamp = new Date().toISOString();
     const challengeMessage = `
-            Challenge: process.env.CHALLENGE_SECRET,
+            Challenge: ${process.env.CHALLENGE_SECRET},
             Wallet Address: ${walletAddress},
             Nonce: ${nonce},
             Timestamp: ${timestamp}
-        `.replace(/\\s+/g, ' ').trim();
-    res.status(200).json({nonce, challengeMessage})
+        `
+      .replace(/\\s+/g, ' ')
+      .trim();
+    res.status(200).json({ nonce, challengeMessage });
   } catch (err) {
-    throwInternalError(res, 'Error generating challenge message: ', err)
+    throwInternalError(res, 'Error generating challenge message: ', err);
   }
-})
+});
 
 router.post('/verify-token', async (req, res) => {
-  const {token} = req.body
+  const { token } = req.body;
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET!)
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!);
 
-    const id: number = (decoded as Request)['user'].id
+    const id: number = (decoded as Request)['user'].id;
 
-    const user = await usersRepo.findOne({where: {uuid: id}})
+    const user = await usersRepo.findOne({ where: { uuid: id } });
 
     if (!user) {
-      res.status(401).json({error: "Invalid token"})
+      res.status(401).json({ error: 'Invalid token' });
       return;
     }
 
-    res.status(200).send()
+    res.status(200).send();
   } catch (err) {
-    res.status(401).send('Invalid token')
+    res.status(401).send('Invalid token');
   }
-})
+});
 
-export default router
+export default router;
